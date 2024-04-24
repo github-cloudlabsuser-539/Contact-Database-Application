61c0280d9770d2a3dc4560a1eb1eb7f2701c7e54
Dominik Brunner <github_cloudlabsuser_539@cloudlabsaiuser.com>
2024-04-24 13:52:02 +0000

    implementation added

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -10,26 +10,42 @@ namespace CRUD_application_2.Controllers
         // GET: User
         public ActionResult Index()
         {
-            // Implement the Index method here
+            // This method is responsible for displaying the list of users.
+            // It retrieves the list of users from the userlist and passes it to the Index view.
+            return View(userlist);
         }
  
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+            // This method is responsible for displaying the details of a specific user with the specified ID.
+            // It retrieves the user from the userlist based on the provided ID and passes it to the Details view.
+            return View(userlist.FirstOrDefault(x => x.Id == id));
         }
  
         // GET: User/Create
         public ActionResult Create()
         {
-            //Implement the Create method here
+            // This method is responsible for displaying the view to create a new user.
+            User user = new User();
+            return View(user);
         }
  
         // POST: User/Create
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+
+            // This method is responsible for handling the HTTP POST request to create a new user.
+            // It receives user input from the form submission and adds the new user to the userlist.
+            // If successful, it redirects to the Index action to display the updated list of users.
+            // If an error occurs during the process, it returns the Create view to display any validation errors.
+            if (ModelState.IsValid)
+            {
+                userlist.Add(user);
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
  
         // GET: User/Edit/5
@@ -37,6 +53,7 @@ namespace CRUD_application_2.Controllers
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            return View(userlist.FirstOrDefault(x => x.Id == id));
         }
  
         // POST: User/Edit/5
@@ -48,12 +65,25 @@ namespace CRUD_application_2.Controllers
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            if (ModelState.IsValid)
+            {
+                var existingUser = userlist.FirstOrDefault(x => x.Id == id);
+                if (existingUser != null)
+                {
+                    existingUser.Name = user.Name;
+                    existingUser.Email = user.Email;
+                    return RedirectToAction("Index");
+                }
+                return HttpNotFound();
+            }
+            return View(user);
         }
  
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             // Implement the Delete method here
+            return View(userlist.FirstOrDefault(x => x.Id == id));
         }
  
         // POST: User/Delete/5
@@ -61,6 +91,14 @@ namespace CRUD_application_2.Controllers
         public ActionResult Delete(int id, FormCollection collection)
         {
             // Implement the Delete method (POST) here
+            var user = userlist.FirstOrDefault(x => x.Id == id);
+            if (user != null)
+            {
+                userlist.Remove(user);
+                return RedirectToAction("Index");
+            }
+            return HttpNotFound();
+        
         }
     }
 }
